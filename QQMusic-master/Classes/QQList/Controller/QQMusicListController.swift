7d4df9132ad02ff7b4255565dd4a3694f7f107d3
Mingriweiji-github <2822072760@qq.com>
2017-09-11 17:24:19 +0800

    resources

--- QQMusic-master/Classes/QQList/Controller/QQMusicListController.swift
+++ QQMusic-master/Classes/QQList/Controller/QQMusicListController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class QQMusicListController: UITableViewController {
-    var musicModel = [QQMusicModel](){
+    var models = [QQMusicModel](){
         didSet{
             tableView.reloadData()
         }
@@ -17,25 +17,27 @@ class QQMusicListController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        setupUI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupUI(){
+        tableView.rowHeight = 60
+        tableView.backgroundView = UIImageView(image: UIImage(named: "QQListBack.jpg"))
+        tableView.separatorStyle = .none
     }
-    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return musicModel.count
+        return models.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellID = "musicCellID"
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! QQMusicCell
+        cell.musicModel = models[indexPath.row]
+        //关键帧缩放动画
+        cell.animation(AnimationType.scale)
         return cell
     }
+    
 
 }
--- QQMusic-master/Classes/QQList/View/QQMusicCell.swift
+++ QQMusic-master/Classes/QQList/View/QQMusicCell.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+enum AnimationType {
+    case rotation
+    case transition
+    case scale
+}
 
 class QQMusicCell: UITableViewCell {
 
     @IBOutlet weak var headerImageV: UIImageView!
     @IBOutlet weak var singerNameLabel: UILabel!
     @IBOutlet weak var songNameLabel: UILabel!
+    var musicModel: QQMusicModel? {
+        didSet{
+            if ((musicModel?.singerImage!) != nil) {
+                headerImageV.image = UIImage(named: (musicModel?.singerImage)!)
+            }
+            singerNameLabel.text = musicModel?.singer
+            songNameLabel.text = musicModel?.name
+        }
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,6 +36,25 @@ class QQMusicCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
+    }
+    
+    func animation(_ type:AnimationType){
+        if type == .rotation{
+            self.layer.removeAnimation(forKey: ".rotation")
+            let animation = CAKeyframeAnimation(keyPath: "transform.rotation.z")
+            animation.values = [M_PI,0]
+            animation.duration = 0.5
+            animation.repeatCount = 1
+            self.layer.add(animation, forKey: "rotation")
+        }
+        if type == .scale{
+            self.layer.removeAnimation(forKey: "scale")
+            let animation = CAKeyframeAnimation(keyPath: "transform.scale.x")
+            animation.values = [0.3,0.5,0.8,0.1]
+            animation.duration = 1
+            animation.repeatCount = 1
+            self.layer.add(animation, forKey: "scale")
+        }
     }
     
 }
