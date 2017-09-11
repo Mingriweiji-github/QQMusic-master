f096cee849d654d14dbc5bc667a9b3bb2f9668b6
Mingriweiji-github <2822072760@qq.com>
2017-09-11 16:56:22 +0800

    cell xib

--- QQMusic-master/Classes/QQList/View/QQMusicCell.swift
+++ QQMusic-master/Classes/QQList/View/QQMusicCell.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class QQMusicCell: UITableViewCell {
 
+    @IBOutlet weak var headerImageV: UIImageView!
+    @IBOutlet weak var singerNameLabel: UILabel!
+    @IBOutlet weak var songNameLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -18,7 +22,6 @@ class QQMusicCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
     
 }
